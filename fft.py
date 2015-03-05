1570a6985f6ca50bccffe8d2bf0b66130299e33c
Nelson Pollard <extern.nelson.pollard@vw.com>
2015-03-05 15:59:58 -0800

    de-normalized fft

--- fft.py
+++ fft.py
@@ -1,4 +1,4 @@
-from pylab import plot, show, title, xlabel, ylabel, subplot, savefig
+from pylab import plot, show, title, axis, xlabel, ylabel, subplot, savefig
 from scipy import fft, arange, ifft
 from numpy import sin, linspace, pi
 from scipy.io.wavfile import read,write
@@ -10,16 +10,16 @@ def plotSpectru(y,Fs):
  frq = k/T # two sides frequency range
  frq = frq[range(n/2)] # one side frequency range
 
- Y = fft(y)/n # fft computing and normalization
+ Y = abs(fft(y)) # fft computing and normalization
  Y = Y[range(n/2)]
  
- plot(frq,abs(Y),'r') # plotting the spectrum
+ plot(frq,Y,'r') # plotting the spectrum
+ axis([0,3000,0,6000])
  xlabel('Freq (Hz)')
  ylabel('|Y(freq)|')
 
-
 Fs,data=read('test.wav')
-y=data[Fs*110:,1]
+y=data[:,1]
 lungime=len(y)
 timp=len(y)/44100.
 t=linspace(0,timp,len(y))
