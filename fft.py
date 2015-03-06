831208b5a8d76fc11d480afb74fa2c9fe8d67e22
Nelson Pollard <extern.nelson.pollard@vw.com>
2015-03-06 15:40:51 -0800

    print 1350

--- fft.py
+++ fft.py
@@ -5,29 +5,14 @@ from scipy.io.wavfile import read,write
 
 def plotAudio(data, fs, time):
  y=data[fs * time:fs * (time + 1),1]
- 
- t = linspace(time, time + 1, y.size)
- subplot(2, 1, 1)
- plot(t, y)
- xlabel('Time')
- ylabel('Amplitude')
- 
- subplot(2, 1, 2)
  Y = abs(fft(y))
- plot(Y)
- printify(Y)
- axis([0,3000,0,1000])
- xlabel('Freq (Hz)')
- ylabel('|Y(freq)|')
- show()
-
-def printify(Y):
- l = []
- for i in range(1,Y.size - 1):
-  if Y[i] > 200:
-    l.append(i)
- print l
+ print time, ": ", Y[1350]
   
 
 (fs,data) = read('test.wav')
+duration = data[:,1].size/fs
+print "time:", duration, "seconds"
 
+for t in range(1, duration - 2):
+ plotAudio(data, fs, t)
+ 
