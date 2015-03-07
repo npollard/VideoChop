7fe3ee2d8d92e13a5f533ae16c8a23271d4bad6f
Nelson Pollard <extern.nelson.pollard@vw.com>
2015-03-06 16:09:10 -0800

    ~670, 1340, 2020, printRanking

--- fft.py
+++ fft.py
@@ -15,7 +15,7 @@ def plotAudio(data, fs, time):
  
  subplot(2, 1, 2)
  plot(abs(fft(y)))
- axis([0,3000,0,6000])
+ axis([0,3000,0,1000])
  xlabel('Freq (Hz)')
  ylabel('|Y(freq)|')
  show()
@@ -23,11 +23,18 @@ def plotAudio(data, fs, time):
 def printAudio(data, fs, time):
  y=data[fs * time:fs * (time + 1),1]
  Y = abs(fft(y))
- print time, ": ", Y[1350]
+ print time, ": ", Y[1360]
 
-def printAll(data, fs):
+def printRanking(data, fs):
+ l = []
  for t in range(1, duration - 2):
-  printAudio(data, fs, t)
+  y=data[fs * t:fs * (t + 1),1]
+  Y = abs(fft(y))
+  l.append([t, sum(Y[670:680])/10 + sum(Y[1340:1360])/20 + sum(Y[2000:2050])/50])
+ 
+ l.sort(key = lambda tup: tup[1])
+ for i in l:
+  print i
 
 (fs,data) = read('test.wav')
 duration = data[:,1].size/fs
