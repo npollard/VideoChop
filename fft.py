75dd8161bc963ebd3b96e2ac958486b0281218bf
Nelson Pollard <extern.nelson.pollard@vw.com>
2015-03-06 16:58:00 -0800

    print times over threshold

--- fft.py
+++ fft.py
@@ -25,16 +25,13 @@ def printAudio(data, fs, time):
  Y = abs(fft(y))
  print time, ": ", Y[1360]
 
-def printRanking(data, fs):
+def rank(data, fs, threshold):
  l = []
  for t in range(1, duration - 2):
   y=data[fs * t:fs * (t + 1),1]
   Y = abs(fft(y))
   l.append([t, sum(Y[670:680])/10 + sum(Y[1340:1360])/20 + sum(Y[2000:2050])/50])
- 
- l.sort(key = lambda tup: tup[1])
- for i in l:
-  print i
+ print "airhorn detected:", map(lambda y: y[0], filter(lambda x: x[1] > threshold, l))
 
 (fs,data) = read('test.wav')
 duration = data[:,1].size/fs
