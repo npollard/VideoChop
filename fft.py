6610af624054b380db65c74e5469da4ee7aee5f4
Nelson Pollard <extern.nelson.pollard@vw.com>
2015-03-06 20:10:12 -0800

    filter for single frequency tone

--- fft.py
+++ fft.py
@@ -4,7 +4,7 @@ from numpy import sin, linspace, pi
 from scipy.io.wavfile import read,write
 
 def plotAudio(data, fs, time):
- y=data[fs * time:fs * (time + 1),1]
+ y=data[fs * time:fs * (time + 1)]
  Y = abs(fft(y))
  
  t = linspace(time, time + 1, y.size)
@@ -15,25 +15,48 @@ def plotAudio(data, fs, time):
  
  subplot(2, 1, 2)
  plot(abs(fft(y)))
- axis([0,3000,0,1000])
+ #axis([0,3000,0,1000])
  xlabel('Freq (Hz)')
  ylabel('|Y(freq)|')
  show()
 
-def printAudio(data, fs, time):
- y=data[fs * time:fs * (time + 1),1]
- Y = abs(fft(y))
- print time, ": ", Y[1360]
 
-def rank(data, fs, threshold):
+def rate(data, fs):
+ duration = data.size/fs
+ print "time:", duration, "seconds"
+ r00 = 670
+ r01 = 680
+ r10 = 1340
+ r11 = 1360
+ r20 = 2000
+ r21 = 2050
  l = []
  for t in range(1, duration - 2):
-  y=data[fs * t:fs * (t + 1),1]
+  y=data[fs * t:fs * (t + 1)]
   Y = abs(fft(y))
-  l.append([t, sum(Y[670:680])/10 + sum(Y[1340:1360])/20 + sum(Y[2000:2050])/50])
- print "airhorn detected:", map(lambda y: y[0], filter(lambda x: x[1] > threshold, l))
+  l.append([t, sum(Y[r00:r01])/(r01 - r00) + sum(Y[r10:r11])/(r11 - r10) + sum(Y[r21:r20])/(r21 - r20)])
+ print "ranges: [" + str(r00) + ":" + str(r01) + "] [" + str(r10) + ":" + str(r11) + "] [" + str(r20) + ":" + str(r21) + "]"
+ return l
 
-(fs,data) = read('test.wav')
-duration = data[:,1].size/fs
-print "time:", duration, "seconds"
+def rateSingle(data, fs, freq):
+ duration = data.size/fs
+ print "time:", duration, "seconds"
+ l = []
+ for t in range(1, duration - 2):
+  y=data[fs * t:fs * (t + 1)]
+  Y = abs(fft(y))
+  l.append([t, Y[freq]])
+ return l
+
+
+def sortTimes(l):
+ for t in sorted(l, key = lambda x: x[1]):
+   print t
+
+def filterTimes(l, threshold):
+ print "threshold: " + str(threshold)
+ print "times:", map(lambda y: y[0], filter(lambda x: x[1] > threshold, l))
+
+
+(fs,data) = read('rec_ub.wav')
 
