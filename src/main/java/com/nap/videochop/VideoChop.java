37b1ce3062cc4be91cd4f9973bbbf38478ac01cb
Nelson Pollard <extern.nelson.pollard@vw.com>
2016-02-03 19:20:04 -0800

    compiles

--- src/main/java/com/nap/videochop/VideoChop.java
+++ src/main/java/com/nap/videochop/VideoChop.java
@@ -1,15 +1,37 @@
 package com.nap.videochop;
 
-import org.bytedeco.javacv.Frame;
-import org.bytedeco.javacv.FFmpegFrameGrabber;
+import java.io.*;
 
 public class VideoChop {
+
+    // http://www.javaworld.com/article/2071275/core-java/when-runtime-exec---won-t.html
+    private class StreamGobbler extends Thread {
+        InputStream inputStream;
+        String type;
+
+        public StreamGobbler(InputStream inputStream, String type) {
+            this.inputStream = inputStream;
+            this.type = type;
+        }
+
+        public void run() {
+            try {
+
+            } catch (Exception e) {
+                System.err.println("ERROR: " + e.getLocalizedMessage());
+            }
+        }
+    }
     
     private static class VideoChopper {
 
         public static void chop(String inputFile) {
-            FFmpegFrameGrabber grabber = new FFmpegFrameGrabber(inputFile);
-            System.out.println(inputFile + ": CHOP!");
+            Runtime rt = Runtime.getRuntime();
+            try {
+                rt.exec("ffmpeg -i ~/Desktop/20160112_161809.mp4 -vf 'blackdetect=d=.2:pix_th=.5' -an -f null -");
+            } catch (IOException e) {
+                System.err.println("ERROR: " + e.getLocalizedMessage());
+            }
         }
 
     }
