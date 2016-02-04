73fc8b6dc9650e8cc04c5cf14223b895a4d9da2a
Nelson Pollard <extern.nelson.pollard@vw.com>
2016-02-04 13:18:48 -0800

    error opening filters

--- src/main/java/com/nap/videochop/VideoChop.java
+++ src/main/java/com/nap/videochop/VideoChop.java
@@ -5,7 +5,7 @@ import java.io.*;
 public class VideoChop {
 
     // http://www.javaworld.com/article/2071275/core-java/when-runtime-exec---won-t.html
-    private class StreamGobbler extends Thread {
+    private static class StreamGobbler extends Thread {
         InputStream inputStream;
         String type;
 
@@ -16,8 +16,11 @@ public class VideoChop {
 
         public void run() {
             try {
-
-            } catch (Exception e) {
+                InputStreamReader inputStreamReader = new InputStreamReader(inputStream);
+                BufferedReader bufferedReader = new BufferedReader(inputStreamReader);
+                String line = null;
+                while ((line = bufferedReader.readLine()) != null) System.out.println(type + "> " + line);
+            } catch (IOException e) {
                 System.err.println("ERROR: " + e.getLocalizedMessage());
             }
         }
@@ -25,10 +28,16 @@ public class VideoChop {
     
     private static class VideoChopper {
 
-        public static void chop(String inputFile) {
-            Runtime rt = Runtime.getRuntime();
+        public void chop() {
+            Runtime runtime = Runtime.getRuntime();
             try {
-                rt.exec("ffmpeg -i ~/Desktop/20160112_161809.mp4 -vf 'blackdetect=d=.2:pix_th=.5' -an -f null -");
+                Process blackDetectProc = runtime.exec("ffmpeg -i /home/nelson/Desktop/20160112_161809.mp4 -vf 'blackdetect=d=.2:pix_th=.5' -an -f null -");
+                StreamGobbler outGobbler = new StreamGobbler(blackDetectProc.getInputStream(), "STDOUT");
+                StreamGobbler errGobbler = new StreamGobbler(blackDetectProc.getErrorStream(), "STDERR");
+                outGobbler.start();
+                errGobbler.start();
+ 
+                
             } catch (IOException e) {
                 System.err.println("ERROR: " + e.getLocalizedMessage());
             }
@@ -36,8 +45,9 @@ public class VideoChop {
 
     }
 
-    public static void main( String[] args ) {
-        VideoChopper.chop("filename");
+    public static void main(String[] args) {
+        VideoChopper videoChopper = new VideoChopper();
+        videoChopper.chop();
 
     }
 
